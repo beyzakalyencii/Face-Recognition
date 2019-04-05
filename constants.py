0fec150365929e638a06d1fc6d9cb7fd20ceadfe
beyzakalyenci <48646012+beyzakalyencii@users.noreply.github.com>
2019-04-05 21:16:00 +0300

    Artificial Intelligent Project

---
+++ constants.py
@@ -0,0 +1,11 @@
+
+CASC_PATH = '/C:/Users/Sercan/smile_project/haarcascade_files/haarcascade_frontalface_default.xml'
+SIZE_FACE = 48
+EMOTIONS = ['angry', 'disgusted', 'fearful',
+            'happy', 'sad', 'surprised', 'neutral']
+SAVE_DIRECTORY = '/C:/Users/Sercan/Desktop/emotion-recognition-neural-networks-master/data/data/'
+SAVE_MODEL_FILENAME = 'Gudi_model_100_epochs_20000_faces'
+DATASET_CSV_FILENAME = 'fer2013.csv'
+SAVE_DATASET_IMAGES_FILENAME = 'data_images.npy'
+SAVE_DATASET_LABELS_FILENAME = 'data_labels.npy'# -*- coding: utf-8 -*-
+
